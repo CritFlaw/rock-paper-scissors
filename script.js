ce685687d4d20e72883f04fbdfca56a20794fc6a
Ted Mihu <ted.mihu@gmail.com>
2023-06-11 14:26:01 +0300

    Changed layout, added game over modal with buttons, not fully functional yet, mostly placeholder. added game over condition. Changed a bit of game logic to not change opacity of score if opacity has already been changed. Changed animation orientation.

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ const scoreText = document.querySelectorAll(".score--text");
 const humanScoreText = document.querySelector(".human--score");
 const pcScoreText = document.querySelector(".computer--score--text");
 const humScoreText = document.querySelector(".human--score--text");
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".result--modal");
+const resultTextModal = document.querySelector(".result--modal--text");
+const modalBtns = document.querySelectorAll(".modal--buttons");
+const modalBtnsContainer = document.querySelector(".game--over--options");
+
+modalBtnsContainer.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("modal--buttons")) {
+    if (e.target.classList.contains("yes--please")) {
+    } else if (e.target.classList.contains("no--im--good")) {
+      console.log(`No I'm good`);
+      console.log(e.target.classList);
+      overlay.classList.add("hidden");
+      overlay.classList.remove("shown");
+      modal.classList.add("hidden");
+      modal.classList.remove("shown");
+    }
+  }
+});
 
 const computerText = document.querySelector(".computer--text");
 // computerText.textContent = ``;
@@ -33,63 +53,86 @@ const computerChoice = function () {
 const tie = "Draw";
 const win = "You won";
 const lose = "You lose";
+let winner = "";
 let result = "";
 let pcScore = 0;
 let humScore = 0;
+let gameOverState = false;
 
 interface.addEventListener("click", function (e) {
   if (e.target.classList.contains("btn")) {
     scoreText.forEach((el) => {
-      el.style.opacity = 1;
+      if ((el.style.opacity = " ")) el.style.opacity = 1;
     });
+    const [hum, pc] = choiceTexts;
     choiceTexts.forEach((el) => {
       el.textContent = "✊";
-      el.classList.add("shake");
+      // el.classList.add("shake");
     });
-    const shakeWatcher = document.querySelector(".shake");
+    hum.classList.add("shake--hum");
+    pc.classList.add("shake--pc");
+    const shakeWatcher = document.querySelector(".shake--hum");
     shakeWatcher.addEventListener("animationend", (e) => {
       pcChoice.textContent = compChoice;
       humChoiceTxt.textContent = humChoice;
-      choiceTexts.forEach((el) => {
-        el.classList.remove("shake");
-      });
-      resultText.textContent = `${result}`;
+      hum.classList.remove("shake--hum");
+      pc.classList.remove("shake--pc");
+      // choiceTexts.forEach((el) => {
+      //   el.classList.remove("shake");
+      // });
       humScoreText.textContent = humScore;
       pcScoreText.textContent = pcScore;
-      console.log(`Animation has ended`);
+      if (gameOverState === true) {
+        console.log(`Overlay should come on`);
+        resultText.textContent = `Game over, ${winner} wins`;
+        overlay.classList.remove("hidden");
+        overlay.classList.add("shown");
+        modal.classList.remove("hidden");
+        modal.classList.add("shown");
+      } else {
+        console.log(humScore, pcScore);
+        resultText.textContent = `${result}`;
+      }
+
+      // console.log(`Animation has ended`);
     });
-    const compChoice = computerChoice();
-    console.log(`PC: ${compChoice}`);
+    const compChoice = "✋"; //computerChoice();
+    // console.log(`PC: ${compChoice}`);
     const humChoice = e.target.textContent;
-    console.log(`You: ${humChoice}`);
+    // console.log(`You: ${humChoice}`);
 
     if (humChoice === compChoice) {
       result = tie;
-      console.log(`Draw`);
+      // console.log(`Draw`);
     } else if (humChoice === "✊" && compChoice === "✋") {
       result = lose;
       pcScore++;
-      console.log(`Computer wins`);
-    } else if (humChoice === "✋" && compChoice === "✊") {
+    }
+    // console.log(`Computer wins`);
+    else if (humChoice === "✋" && compChoice === "✊") {
       result = win;
       humScore++;
-      console.log(`Human wins`);
+      // console.log(`Human wins`);
     } else if (humChoice === "✌" && compChoice === "✋") {
       result = win;
       humScore++;
-      console.log(`Human wins`);
+      // console.log(`Human wins`);
     } else if (humChoice === "✊" && compChoice === "✌") {
       result = win;
       humScore++;
-      console.log(`Human wins`);
+      // console.log(`Human wins`);
     } else if (humChoice === "✋" && compChoice === "✌") {
       result = lose;
       pcScore++;
-      console.log("Computer wins");
+      // console.log("Computer wins");
     } else if (humChoice === "✌" && compChoice === "✊") {
       result = lose;
       pcScore++;
-      console.log(`Computer wins`);
+      // console.log(`Computer wins`);
+    }
+    if (humScore === 5 || pcScore === 5) {
+      humScore > pcScore ? (winner = "You") : "Computer";
+      gameOverState = true;
     }
   }
 });
